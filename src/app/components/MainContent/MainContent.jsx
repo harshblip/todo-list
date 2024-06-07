d9d0fb9928e5912d1cf668a41e92857adae6d062
harshblip <harsh.jipkate2020@vitbhopal.ac.in>
2024-06-07 23:17:57 +0530

    working on dummy ui

--- src/app/components/MainContent/MainContent.jsx
+++ src/app/components/MainContent/MainContent.jsx
@@ -1,12 +1,43 @@
+import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { ChevronRight, CircleCheck } from "lucide-react";
+import { Ubuntu } from "next/font/google"
+
+const rubik = Ubuntu({
+    subsets: ["latin"],
+    weight: '400',
+});
+
 export default function MainContent() {
     return (
         <>
-            <div className="flex justify-center">
+            <div className="flex">
                 <hr
-                    className="border-gray-200 w-1/2"
+                    className="border-gray-200 mt-3 w-12 ml-20 sm:ml-0"
+                />
+                <p className={`ml-2 text-lg -mt-[0.1rem] ${rubik.className}`}> Important </p>
+                <hr
+                    className="border-gray-200 mt-3 w-full ml-3 sm:mr-0 mr-12"
                 />
             </div>
-            <p className="absolute justify-start"> Important </p>
+            <div className="bg-white/85 flex justify-between">
+                <div className="flex">
+                    <CircleCheck />
+                    <p> fix UI bugs </p>
+                </div>
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="outline" > <ChevronRight /> </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent className="w-56">
+                        <DropdownMenuGroup>
+                            <DropdownMenuItem>
+                                <p> Edit </p>
+                            </DropdownMenuItem>
+                        </DropdownMenuGroup>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </div>
         </>
     )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,16 @@
 import Navbar from "./components/Navbar/layout"
 import MainContent from "./components/MainContent/MainContent"
 
-export default function page  () {
+export default function page() {
   return (
-    <>
-      <Navbar />
-      <MainContent />
-    </>
+    <div className="flex justify-center items-center">
+      <div className="flex flex-col w-[32rem]">
+        <div className="w-[32rem] ">
+
+          <Navbar />
+          <MainContent />
+        </div>
+      </div>
+    </div>
   )
 }
