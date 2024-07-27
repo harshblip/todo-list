54a7035394ab31ebc2d2814cf799771d5ee1958f
harshblip <harsh.jipkate2020@vitbhopal.ac.in>
2024-07-27 20:03:41 +0530

    500 auth error

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,10 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client";
 import prisma from "../../../prisma/prisma";
+
+const prisma = new PrismaClient();
 
 export default NextAuth({
     adapter: PrismaAdapter(prisma),
